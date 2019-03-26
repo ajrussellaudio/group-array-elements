c64799766313170b6bf5a9d8ee7936c7648f4612
Alan Russell <ajrussellaudio@gmail.com>
2019-03-26 21:00:12 +0000

    implement second test

--- src/groupArrayElements.js
+++ src/groupArrayElements.js
@@ -1,5 +1,10 @@
 const groupArrayElements = (array, numberOfChunks) => {
   if (numberOfChunks <= 0) return array;
+  const result = [];
+  for (let i = 0; i < numberOfChunks; i++) {
+    result[i] = array.slice(i);
+  }
+  return result;
 };
 
 module.exports = groupArrayElements;
--- tests/groupArrayElements.test.js
+++ tests/groupArrayElements.test.js
@@ -8,9 +8,13 @@ describe("groupArrayElements", () => {
     assert.deepEqual(result, input);
   });
 
-  it("groups an array into 1 new array when N is 1");
+  it("groups an array into 1 new array when N is 1", () => {
+    const input = [1, 2, 3, 4, 5];
+    const result = groupArrayElements(input, 1);
+    assert.deepEqual(result, [[1, 2, 3, 4, 5]]);
+  });
 
   it("groups an array into 3 new arrays when N is 3");
 
-  it("groups array into `length` arrays when N is greater than `length`");
+  it("groups array into `length` arrays when N is `length`");
 });
