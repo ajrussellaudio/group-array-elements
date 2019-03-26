f9556b7b75f1c9231187ba1358e4eda706930f0e
Alan Russell <ajrussellaudio@gmail.com>
2019-03-26 20:45:31 +0000

    add pending tests

--- tests/sanity.test.js
+++ tests/sanity.test.js
@@ -1,7 +1,11 @@
 const assert = require("assert");
 
-describe("sanity check", () => {
-  it("works", () => {
-    assert.equal(2 + 2, 4);
-  });
+describe("groupArrayElements", () => {
+  it("returns the original array when N is 0 or less");
+
+  it("groups an array into 1 new array when N is 1");
+
+  it("groups an array into 3 new arrays when N is 3");
+
+  it("groups array into `length` arrays when N is greater than `length`");
 });
