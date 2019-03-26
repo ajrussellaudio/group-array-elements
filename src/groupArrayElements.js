cf095860047c9420f3df6176e6d84846aba4d477
Alan Russell <ajrussellaudio@gmail.com>
2019-03-26 22:03:27 +0000

    refactor groupArrayElements

--- src/groupArrayElements.js
+++ src/groupArrayElements.js
@@ -1,12 +1,10 @@
 const groupArrayElements = (array, numberOfChunks) => {
   if (numberOfChunks <= 0) return array;
   const chunkSize = Math.ceil(array.length / numberOfChunks);
-
-  let result = [];
-  for (let i = 0; i < array.length; i += chunkSize) {
-    result.push(array.slice(i, i + chunkSize));
-  }
-  return result;
+  return Array(numberOfChunks)
+    .fill(null)
+    .map((_item, i) => array.slice(i * chunkSize, i * chunkSize + chunkSize))
+    .filter(chunk => chunk.length > 0);
 };
 
 module.exports = groupArrayElements;
